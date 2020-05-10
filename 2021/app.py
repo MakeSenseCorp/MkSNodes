8f35c342cbc44451eddbc4a3fd58bf3c2b4b7492
Yevgeniy Kiveisha <yevgeniy.kiveisha.com>
2020-05-10 14:32:17 +0300

    N/A
    Signed-off-by: Yevgeniy Kiveisha <yevgeniy.kiveisha.com>

--- 2021/app.py
+++ 2021/app.py
@@ -25,11 +25,15 @@ from mksdk import MkSTimer
 from flask import Response, request
 from flask import send_file
 
+import vlc
+import hashlib
+
 class FolderMonitor():
 	def __init__(self, path):
 		self.ClassName					= "FolderMonitor"
 		self.Path 						= path
 		self.Items 						= []
+		self.File 						= MkSFile.File()
 
 		self.WorkerThread				= None
 		self.FolderContentChangedEvent	= None
@@ -45,15 +49,15 @@ class FolderMonitor():
 
 	def GetItemsCompare(self):
 		ret_items = []
-		items = GetItems()
+		items = self.GetItems()
 		if len(self.Items) == 0:
 			for item in items:
 				ret_items.append({
 					"item": item,
 					"status": "append"
 				})
-			self.Items = ret_items
-			return ret_items
+			self.Items = items
+			return ret_items, (len(ret_items) > 0)
 		else:
 			# Find removed items
 			for item in self.Items:
@@ -73,6 +77,14 @@ class FolderMonitor():
 					self.Items.append(item)
 		return ret_items, (len(ret_items) > 0)
 
+class Security():
+	def __init__(self):
+		pass
+	
+	def GetMD5Hash(self, content):
+		return hashlib.md5(content).hexdigest()
+		# For Python 3+ hashlib.md5("whatever your string is".encode('utf-8')).hexdigest()
+
 class Context():
 	def __init__(self, node):
 		self.ClassName					= "Apllication"
@@ -85,6 +97,7 @@ class Context():
 		}
 		# Handlers
 		self.RequestHandlers		= {
+			'get_sensor_info':			self.GetSensorInfoHandler,
 			'player_operation':			self.PlayerOperationHandler,
 			'playlist_operation':		self.PlaylistOperationHandler,
 			'critical_operation':		self.CriticalOperationHandler,
@@ -106,6 +119,7 @@ class Context():
 		self.USBStoragePath 			= "./media/usb"
 		self.SelectedStoragePath 		= None
 		self.SongsFolder				= None
+		self.Player 					= vlc.MediaPlayer()
 
 		self.Timer 						= MkSTimer.MkSTimer()
 		self.Timer.OnTimerTriggerEvent  = self.OnTimerTriggerHandler
@@ -117,11 +131,35 @@ class Context():
 	def UndefindHandler(self, sock, packet):
 		print ("UndefindHandler")
 	
+	def GetSensorInfoHandler(self, sock, packet):
+		print ("({classname})# GetSensorInfoHandler ...".format(classname=self.ClassName))
+		payload = {
+			'playlists': self.DB["playlists"],
+			'songs': self.SongsFolder.GetItemsList()
+		}
+
+		return THIS.Node.BasicProtocol.BuildResponse(packet, payload)
+	
 	def PlayerOperationHandler(self, sock, packet):
 		payload = THIS.Node.BasicProtocol.GetPayloadFromJson(packet)
 		print ("({classname})# PlayerOperationHandler ... payload: {0}".format(payload, classname=self.ClassName))
+
+		if "play" in payload["operation"]:
+			self.Player.stop()
+			self.Player.set_mrl(os.path.join(self.SelectedStoragePath, "songs", payload["song"]["name"]))
+			self.Player.play()
+		elif "stop" in payload["operation"]:
+			self.Player.stop()
+		elif "pause" in payload["operation"]:
+			pass
+		elif "skip_back" in payload["operation"]:
+			pass
+		elif "skip_forward" in payload["operation"]:
+			pass
 		
-		data = {}
+		data = {
+			"error" : "none"
+		}
 		return THIS.Node.BasicProtocol.BuildResponse(packet, data)
 	
 	def PlaylistOperationHandler(self, sock, packet):
@@ -167,12 +205,15 @@ class Context():
 		if not os.path.exists(self.USBStoragePath):
 			os.makedirs(self.USBStoragePath)
 		
-		self.SelectedStoragePath = self.LocalStoragePath
+		if self.LocalStorageEnabled == 1:
+			self.SelectedStoragePath = self.LocalStoragePath
+		else:
+			self.SelectedStoragePath = self.USBStoragePath
 		
-		if not os.path.exists(self.USBStoragePath):
-			os.makedirs(self.USBStoragePath)
+		if not os.path.exists(os.path.join(self.SelectedStoragePath, "songs")):
+			os.makedirs(os.path.join(self.SelectedStoragePath, "songs"))
 		
-		self.SongsFolder = FolderMonitor(os.path("{0}/songs", self.LocalStoragePath))
+		self.SongsFolder = FolderMonitor(os.path.join(self.SelectedStoragePath, "songs"))
 		self.SongsFolder.GetItemsCompare()
 
 		# self.Timer.LoadClocks(addrs)
@@ -233,14 +274,14 @@ class Context():
 				print ("  {0}\t{1}\t{2}\t{3}\t{4}\t{5}".format(str(idx),item.LocalType,item.UUID,item.IP,item.Port,item.Type))
 			print("")
 
+			# Search for change in MP3 folder
 			items, is_change = self.SongsFolder.GetItemsCompare()
-
 			if is_change is True:
 				THIS.Node.EmitOnNodeChange({
-						'event': "media_folder_changed",
-						'data': {
-							'songs': items
-						}
+					'event': "media_folder_changed",
+					'data': {
+						'songs': items
+					}
 				})
 
 Node = MkSSlaveNode.SlaveNode()
