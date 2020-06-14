3407a3103697109727f76f5e5a1c537e3296cfda
Yevgeniy Kiveisha <yevgeniy.kiveisha.com>
2020-06-14 15:39:29 +0300

    N/A
    Signed-off-by: Yevgeniy Kiveisha <yevgeniy.kiveisha.com>

--- master/app.py
+++ master/app.py
@@ -51,7 +51,7 @@ class Context():
 		payload = THIS.Node.Network.BasicProtocol.GetPayloadFromJson(packet)
 		src = THIS.Node.Network.BasicProtocol.GetSourceFromJson(packet)
 
-		if src in THIS.Node.IPScannerServiceUUID:
+		if src in THIS.Node.Services[103]["uuid"]:
 			self.NetworkDevicesList = payload["online_devices"]
 
 		return THIS.Node.Network.BasicProtocol.BuildResponse(packet, {
